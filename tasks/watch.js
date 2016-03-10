0b2ac38f980338b8950cf63afe301dd712ff71e7
Matthew Batchelder <borkweb@gmail.com>
2016-03-10 11:36:49 -0500

    Add livereload and smarter compiling to "watch"

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,36 +1,58 @@
+var cssnano    = require( 'gulp-cssnano' );
 var gulp       = require( 'gulp' );
-var livereload = require( 'gulp-livereload' );
 var gutil      = require( 'gulp-util' );
+var livereload = require( 'gulp-livereload' );
+var rename     = require( 'gulp-rename' );
+var uglify     = require( 'gulp-uglify' );
 
 var watch_task = function() {
 	'use strict';
 
 	livereload.listen();
 
+	// watch for changes to non .min JS files and compress them
 	gulp.watch(
 		[
 		  'src/resources/js/*.js',
 			'!src/resources/js/*.min.js'
 		],
-		[ 'compress-js' ]
+		function( file ) {
+		  gulp.src( file.path )
+			  .pipe( uglify() )
+				.pipe(
+				  rename( {
+					  extname: '.min.js'
+					} )
+				)
+				.pipe( gulp.dest( 'src/resources/js' ) );
+		}
 	);
 
+	// watch for changes to non .min CSS files and compress them
 	gulp.watch(
 		[
 		  'src/resources/css/*.css',
 			'!src/resources/css/*.min.css'
 		],
-		[ 'compress-css' ]
+		function( file ) {
+		  gulp.src( file.path )
+			  .pipe( cssnano() )
+				.pipe(
+				  rename( {
+					  extname: '.min.css'
+					} )
+				)
+				.pipe( gulp.dest( 'src/resources/css' ) );
+		}
 	);
 
-	gulp.watch( 'src/resources/js/*.js' ).on( 'change', function( file ) {
+	// watch for changes to JS and CSS and let livereload know about those changes
+	gulp.watch( [
+	  'src/resources/js/*.js',
+	  'src/resources/css/*.css'
+	] ).on( 'change', function( file ) {
 		livereload.changed( file.path );
-		gutil.log( gutil.colors.yellow( 'JS changed (' + file.path + ')' ) );
-	} );
-
-	gulp.watch( 'src/resources/css/*.css' ).on( 'change', function( file ) {
-		livereload.changed( file.path );
-		gutil.log( gutil.colors.yellow( 'CSS changed (' + file.path + ')' ) );
+		gutil.log( gutil.colors.yellow( 'File changed (' + file.path + ')' ) );
 	} );
 };
 
