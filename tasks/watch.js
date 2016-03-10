ff78019b1dc2abb57a0c00057b964b6fbcfb5e47
Matthew Batchelder <borkweb@gmail.com>
2016-03-10 11:23:57 -0500

    Watch and compile. Watch and livereload

--- tasks/watch.js
+++ tasks/watch.js
@@ -5,12 +5,28 @@ var watch_task = function() {
 
 	livereload.listen();
 
-	gulp.watch( 'src/resources/js/*.js', [ 'compress-js' ] ).on( 'change', function( file ) {
+	gulp.watch(
+		[
+		  'src/resources/js/*.js',
+			'!src/resources/js/*.min.js'
+		],
+		[ 'compress-js' ]
+	);
+
+	gulp.watch(
+		[
+		  'src/resources/css/*.css',
+			'!src/resources/css/*.min.css'
+		],
+		[ 'compress-css' ]
+	);
+
+	gulp.watch( 'src/resources/js/*.js' ).on( 'change', function( file ) {
 		livereload.changed( file.path );
 		gutil.log( gutil.colors.yellow( 'JS changed (' + file.path + ')' ) );
 	} );
 
-	gulp.watch( 'src/resources/css/*.css', [ 'compress-css' ] ).on( 'change', function( file ) {
+	gulp.watch( 'src/resources/css/*.css' ).on( 'change', function( file ) {
 		livereload.changed( file.path );
 		gutil.log( gutil.colors.yellow( 'CSS changed (' + file.path + ')' ) );
 	} );
