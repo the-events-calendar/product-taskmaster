39d4f85d0cd47182ec2762c86953f482c2d94495
Matthew Batchelder <borkweb@gmail.com>
2016-03-10 11:19:13 -0500

    Add livereload and a watch task

---
+++ tasks/watch.js
@@ -0,0 +1,20 @@
+var gulp   = require( 'gulp' );
+
+var watch_task = function() {
+	'use strict';
+
+	livereload.listen();
+
+	gulp.watch( 'src/resources/js/*.js' ).on( 'change', function( file ) {
+		livereload.changed( file.path );
+		gutil.log( gutil.colors.yellow( 'JS changed (' + file.path + ')' ) );
+	} );
+
+	gulp.watch( 'src/resources/css/*.css' ).on( 'change', function( file ) {
+		livereload.changed( file.path );
+		gutil.log( gutil.colors.yellow( 'CSS changed (' + file.path + ')' ) );
+	} );
+};
+
+gulp.task( 'watch', watch_task );
+module.exports = watch_task;
