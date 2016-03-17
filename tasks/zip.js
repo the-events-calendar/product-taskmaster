92ab71b67d964755a4e936b129a65e6bf85c74c9
Matthew Batchelder <borkweb@gmail.com>
2016-03-17 11:48:46 -0400

    Fix zip task to properly nest

--- tasks/zip.js
+++ tasks/zip.js
@@ -1,21 +1,52 @@
-var gulp   = require( 'gulp' );
-var zip = require( 'gulp-vinyl-zip' ).zip;
-var fs  = require( 'fs-sync' );
+var gulp     = require( 'gulp' );
+var fs       = require( 'fs' );
+var zip      = require( 'gulp-vinyl-zip' ).zip;
+var sync     = require( 'fs-sync' );
+var sequence = require( 'run-sequence' ).use( gulp );
 
-var zip_task = function() {
+// this task copies files we'll zip into a build directory
+gulp.task( 'zip-copy-files', function() {
   'use strict';
 
   var json = JSON.parse( fs.readFileSync( './package.json' ) );
   var zip_include = JSON.parse( fs.readFileSync( './package-whitelist.json' ) );
 
-	fs.mkdir( json._zipfoldername );
-	for ( var i in zip_include ) {
-		fs.copy( zip_include[ i ], json._zipfoldername + '/' + zip_include[ i ] );
-	}
+	sync.mkdir( json._zipfoldername );
+	return gulp.src( zip_include, { base: '.' } )
+		.pipe( gulp.dest( json._zipfoldername ) );
+} );
 
-	return gulp.src( json._zipfoldername )
+// this does the zipping
+gulp.task( 'zip-do-zip', function() {
+  'use strict';
+
+  var json = JSON.parse( fs.readFileSync( './package.json' ) );
+
+	return gulp.src( json._zipfoldername + '/**/*', { base: '.' } )
 		.pipe( zip( json._zipname + '.' + json.version + '.zip' ) )
 		.pipe( gulp.dest( '../' ) );
+} );
+
+// this cleans up the trash
+gulp.task( 'zip-purge-build-dir', function( cb ) {
+  'use strict';
+
+  var json = JSON.parse( fs.readFileSync( './package.json' ) );
+
+	sync.remove( json._zipfoldername );
+
+	cb();
+} );
+
+var zip_task = function( cb ) {
+  'use strict';
+
+  sequence(
+    'zip-copy-files',
+    'zip-do-zip',
+    'zip-purge-build-dir',
+    cb
+  );
 };
 
 gulp.task( 'zip', zip_task );
