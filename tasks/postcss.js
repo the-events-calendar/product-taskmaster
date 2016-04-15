fc036738a3415e7d3e7e6ab6a57aff855f0511f5
Matthew Batchelder <borkweb@gmail.com>
2016-04-15 10:34:12 -0400

    Adding a header to our compiled CSS files

--- tasks/postcss.js
+++ tasks/postcss.js
@@ -1,4 +1,5 @@
 var gulp        = require( 'gulp' );
+var header      = require('gulp-header');
 var postcss     = require( 'gulp-postcss' );
 var cssnext     = require( 'postcss-cssnext' );
 var cssimport   = require( 'postcss-import' );
@@ -17,11 +18,24 @@ var postcss_task = function() {
 		cssmqpacker(),
 	];
 
+  var banner = [
+		'/**',
+		' * This CSS file was auto-generated via PostCSS',
+		' *',
+		' * Contributors should avoid editing this file, but instead edit the associated',
+		' * src/resources/postcss/ file. For more information, check out our engineering',
+		' * docs on how we handle CSS in our engineering docs.',
+		' *',
+		' * @see: http://moderntribe.github.io/products-engineering/css/',
+		' */',
+  ].join( '\n' );
+
 	return gulp.src( [
 		'./src/resources/postcss/**/*.css',
 		'!./src/resources/postcss/**/_*.css',
 	] )
 		.pipe( postcss( processors ) )
+		.pipe( header( banner ) )
 		.pipe( gulp.dest( './src/resources/css' ) );
 };
 
