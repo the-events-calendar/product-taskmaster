767490187a348379e0c2b95aff84868c9ddbb606
Matthew Batchelder <borkweb@gmail.com>
2016-04-15 10:44:51 -0400

    Add headers to watch

--- tasks/postcss.js
+++ tasks/postcss.js
@@ -8,37 +8,37 @@ var cssmixins   = require( 'postcss-mixins' );
 var cssmqpacker = require( 'css-mqpacker' );
 
 var postcss_task = function() {
-	'use strict';
+  'use strict';
 
-	var processors = [
-		cssimport(),
-		cssmixins(),
-		cssnested(),
-		cssnext(),
-		cssmqpacker(),
-	];
+  var processors = [
+    cssimport(),
+    cssmixins(),
+    cssnested(),
+    cssnext(),
+    cssmqpacker(),
+  ];
 
   var banner = [
-		'/**',
-		' * This CSS file was auto-generated via PostCSS',
-		' *',
-		' * Contributors should avoid editing this file, but instead edit the associated',
-		' * src/resources/postcss/ file. For more information, check out our engineering',
-		' * docs on how we handle CSS in our engineering docs.',
-		' *',
-		' * @see: http://moderntribe.github.io/products-engineering/css/',
-		' */',
-		'',
-		'',
+    '/**',
+    ' * This CSS file was auto-generated via PostCSS',
+    ' *',
+    ' * Contributors should avoid editing this file, but instead edit the associated',
+    ' * src/resources/postcss/ file. For more information, check out our engineering',
+    ' * docs on how we handle CSS in our engineering docs.',
+    ' *',
+    ' * @see: http://moderntribe.github.io/products-engineering/css/',
+    ' */',
+    '',
+    '',
   ].join( '\n' );
 
-	return gulp.src( [
-		'./src/resources/postcss/**/*.css',
-		'!./src/resources/postcss/**/_*.css',
-	] )
-		.pipe( postcss( processors ) )
-		.pipe( header( banner ) )
-		.pipe( gulp.dest( './src/resources/css' ) );
+  return gulp.src( [
+    './src/resources/postcss/**/*.css',
+    '!./src/resources/postcss/**/_*.css',
+  ] )
+    .pipe( postcss( processors ) )
+    .pipe( header( banner ) )
+    .pipe( gulp.dest( './src/resources/css' ) );
 };
 
 gulp.task( 'postcss', postcss_task );
--- tasks/watch.js
+++ tasks/watch.js
@@ -1,10 +1,11 @@
-var cssnano    = require( 'gulp-cssnano' );
-var fs         = require( 'fs' );
-var gulp       = require( 'gulp' );
-var gutil      = require( 'gulp-util' );
-var livereload = require( 'gulp-livereload' );
-var rename     = require( 'gulp-rename' );
-var uglify     = require( 'gulp-uglify' );
+var cssnano     = require( 'gulp-cssnano' );
+var fs          = require( 'fs' );
+var gulp        = require( 'gulp' );
+var gutil       = require( 'gulp-util' );
+var livereload  = require( 'gulp-livereload' );
+var rename      = require( 'gulp-rename' );
+var uglify      = require( 'gulp-uglify' );
+var header      = require('gulp-header');
 var postcss     = require( 'gulp-postcss' );
 var cssnext     = require( 'postcss-cssnext' );
 var cssimport   = require( 'postcss-import' );
@@ -13,9 +14,9 @@ var cssmixins   = require( 'postcss-mixins' );
 var cssmqpacker = require( 'css-mqpacker' );
 
 var watch_task = function() {
-	'use strict';
+  'use strict';
 
-	livereload.listen();
+  livereload.listen();
 
   var postcss_dir = 'src/resources/postcss';
   var css_dir = 'src/resources/css';
@@ -33,73 +34,88 @@ var watch_task = function() {
     js_dir = 'resources';
   }
 
-	// watch for changes to postcss files and compile them
-	gulp.watch(
-		[
-		  postcss_dir + '/*.css',
-		],
-		function() {
-			var processors = [
-				cssimport(),
-				cssmixins(),
-				cssnested(),
-				cssnext(),
-				cssmqpacker(),
-			];
+  // watch for changes to postcss files and compile them
+  gulp.watch(
+    [
+      postcss_dir + '/*.css',
+    ],
+    function() {
+      var processors = [
+        cssimport(),
+        cssmixins(),
+        cssnested(),
+        cssnext(),
+        cssmqpacker(),
+      ];
 
-			gulp.src( [
-				'./src/resources/postcss/**/*.css',
-				'!./src/resources/postcss/**/_*.css',
-			] )
-				.pipe( postcss( processors ) )
-				.pipe( gulp.dest( './src/resources/css' ) );
-		}
-	);
+      var banner = [
+        '/**',
+        ' * This CSS file was auto-generated via PostCSS',
+        ' *',
+        ' * Contributors should avoid editing this file, but instead edit the associated',
+        ' * src/resources/postcss/ file. For more information, check out our engineering',
+        ' * docs on how we handle CSS in our engineering docs.',
+        ' *',
+        ' * @see: http://moderntribe.github.io/products-engineering/css/',
+        ' */',
+        '',
+        '',
+      ].join( '\n' );
 
-	// watch for changes to non .min JS files and compress them
-	gulp.watch(
-		[
-		  js_dir + '/*.js',
-			'!' + js_dir + '/*.min.js'
-		],
-		function( file ) {
-		  gulp.src( file.path )
-			  .pipe( uglify() )
-				.pipe(
-				  rename( {
-					  extname: '.min.js'
-					} )
-				)
-				.pipe( gulp.dest( js_dir ) );
-		}
-	);
+      gulp.src( [
+        './src/resources/postcss/**/*.css',
+        '!./src/resources/postcss/**/_*.css',
+      ] )
+        .pipe( postcss( processors ) )
+        .pipe( header( banner ) )
+        .pipe( gulp.dest( './src/resources/css' ) );
+    }
+  );
 
-	// watch for changes to non .min CSS files and compress them
-	gulp.watch(
-		[
-		  css_dir + '/*.css',
-			'!' + css_dir + '/*.min.css'
-		],
-		function( file ) {
-		  gulp.src( file.path )
-			  .pipe( cssnano() )
-				.pipe(
-				  rename( {
-					  extname: '.min.css'
-					} )
-				)
-				.pipe( gulp.dest( css_dir ) );
-		}
-	);
+  // watch for changes to non .min JS files and compress them
+  gulp.watch(
+    [
+      js_dir + '/*.js',
+      '!' + js_dir + '/*.min.js'
+    ],
+    function( file ) {
+      gulp.src( file.path )
+        .pipe( uglify() )
+        .pipe(
+          rename( {
+            extname: '.min.js'
+          } )
+        )
+        .pipe( gulp.dest( js_dir ) );
+    }
+  );
 
-	// watch for changes to JS and CSS and let livereload know about those changes
-	gulp.watch( [
-	  js_dir + '/*.js',
-	  css_dir + '/*.css'
-	] ).on( 'change', function( file ) {
-		livereload.changed( file.path );
-		gutil.log( gutil.colors.yellow( 'File changed (' + file.path + ')' ) );
-	} );
+  // watch for changes to non .min CSS files and compress them
+  gulp.watch(
+    [
+      css_dir + '/*.css',
+      '!' + css_dir + '/*.min.css'
+    ],
+    function( file ) {
+      gulp.src( file.path )
+        .pipe( cssnano() )
+        .pipe(
+          rename( {
+            extname: '.min.css'
+          } )
+        )
+        .pipe( gulp.dest( css_dir ) );
+    }
+  );
+
+  // watch for changes to JS and CSS and let livereload know about those changes
+  gulp.watch( [
+    js_dir + '/*.js',
+    css_dir + '/*.css'
+  ] ).on( 'change', function( file ) {
+    livereload.changed( file.path );
+    gutil.log( gutil.colors.yellow( 'File changed (' + file.path + ')' ) );
+  } );
 };
 
 gulp.task( 'watch', watch_task );
