da34a25ab0957f54f6514c3cc8d0d744892d0a8c
bordoni <bordoni.dev@gmail.com>
2016-12-08 23:05:03 -0200

    Receive Gulp from Parent Module + Prevent Common To be included + Save .pot to the correct place on all plugins

--- tasks/pot.js
+++ tasks/pot.js
@@ -1,36 +1,50 @@
-var gulp    = require( 'gulp' );
-var fs      = require( 'fs' );
-var wppot   = require( 'gulp-wp-pot' );
-var sort    = require( 'gulp-sort' );
-
 var pot_task = function() {
-  'use strict';
+	'use strict';
+	var gulp    = require( 'gulp' ),
+		fs      = require( 'fs' ),
+		wppot   = require( 'gulp-wp-pot' ),
+		sort    = require( 'gulp-sort' );
 
-  var json = JSON.parse( fs.readFileSync( './package.json' ) );
-  var readme = fs.readFileSync( './readme.txt' );
-	var plugin_name = /=== (.*) ===/.exec( readme );
-	plugin_name = plugin_name[1];
-	var version = /Stable tag: (.*)/.exec( readme );
-	version = version[1];
+	var json   = JSON.parse( fs.readFileSync( './package.json' ) ),
+		readme = './readme.txt',
+		label  = json.name;
+
+	if ( fs.existsSync( readme ) ) {
+		readme = fs.readFileSync( readme ),
+		label  = /=== (.*) ===/.exec( readme )[1];
+	}
 
 	return gulp.src( [
-		'**/*.php',
-		'!common/*',
-		'!lang/*',
-		'!tests/*',
-		'!vendor/*'
-	] )
+			'**/*.php',
+			'!common/*',
+			'!lang/*',
+			'!tests/*',
+			'!vendor/*'
+		] )
 		.pipe( sort() )
 		.pipe(
 			wppot( {
-				package: plugin_name + ' ' + version,
-				destFile: json._glotPressSlug + '.pot',
+				package: label + ' ' + json.version,
+				destFile: json.name + '.pot',
 				domain: json._textDomain,
-				bugReport: 'http://m.tri.be/191x'
+				bugReport: 'http://m.tri.be/191x',
+				headers: {
+					'X-Generated-Time': ( new Date() ).toJSON(),
+					'X-Poedit-Basepath': '..',
+					'X-Poedit-SourceCharset': 'UTF-8',
+					'X-Poedit-KeywordsList': '__;_e;_n:1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;esc_attr__;esc_attr_e;esc_attr_x:1,2c;esc_html__;esc_html_e;esc_html_x:1,2c;_n_noop:1,2;_nx_noop:3c,1,2;__ngettext_noop:1,2',
+					'X-Poedit-SearchPath-0': '.',
+					'X-Poedit-SearchPathExcluded-0': '*.js',
+					'X-Poedit-SearchPathExcluded-1': 'common',
+					'X-Poedit-SearchPathExcluded-2': 'lang',
+					'X-Poedit-SearchPathExcluded-3': 'tests',
+					'X-Poedit-SearchPathExcluded-4': 'vendor',
+				}
 			} )
 		)
 		.pipe( gulp.dest( 'lang' ) );
 };
 
-gulp.task( 'pot', pot_task );
-module.exports = pot_task;
+module.exports = function( gulp ) {
+	gulp.task( 'pot', pot_task );
+};
