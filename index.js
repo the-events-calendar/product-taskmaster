656fb62442ed3a0c964205c5b87df74ab1212434
bordoni <bordoni.dev@gmail.com>
2016-12-08 23:03:57 -0200

    Ditch Require-Dir because it wouldn't allow passing params to the module

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = function() {
-	require( 'require-dir' )( './tasks', { recurse: true } );
+module.exports = function( gulp ) {
+	var normalizedPath = require( 'path' ).join( __dirname, 'tasks' );
+
+	require( 'fs' ).readdirSync( normalizedPath ).forEach( function( file ) {
+		require( './tasks/' + file )( gulp );
+	} );
+	// require( 'require-dir' )( './tasks', { recurse: true } );
 };
