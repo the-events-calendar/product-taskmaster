b470729d945c17751c3199c75c596669fc403e03
Neal Fennimore <hi@neal.codes>
2018-11-09 14:36:55 -0500

    Linter fixes

--- scripts/fetchPackageJSON.js
+++ scripts/fetchPackageJSON.js
@@ -1,22 +1,23 @@
-const https = require('https');
-const {createReadStream} = require('fs');
-const {resolve} = require('path');
+/* eslint-disable max-len, no-console */
+const https = require( 'https' );
 
-module.exports = new Promise((resolve, reject)=>{
-    // TODO: Change to use master branch
-    https.get('https://raw.githubusercontent.com/moderntribe/tribe-common/task/gutenberg-merge/package.json', response => {
-        let data = '';
-        response.setEncoding('utf8');
-        response.on('data', (chunk) => data += chunk );
-        response.on('abort', reject );
-        response.on('end', () => {
-            try {
-                resolve( JSON.parse(data) );
-                console.info('Successfully fetched latest tribe-common package.json');
-            } catch (e) {
-                reject(e);
-                console.error(e);
-            }
-        } );
-    });
-});
+// TODO: Change to use master branch
+const URL = 'https://raw.githubusercontent.com/moderntribe/tribe-common/task/gutenberg-merge/package.json';
+
+module.exports = new Promise( ( resolve, reject ) => {
+	https.get( URL, response => {
+		let data = '';
+		response.setEncoding( 'utf8' );
+		response.on( 'data', ( chunk ) => data += chunk );
+		response.on( 'abort', reject );
+		response.on( 'end', () => {
+			try {
+				resolve( JSON.parse( data ) );
+				console.info( 'Successfully fetched latest tribe-common package.json' );
+			} catch ( e ) {
+				reject( e );
+				console.error( e );
+			}
+		} );
+	} );
+} );
