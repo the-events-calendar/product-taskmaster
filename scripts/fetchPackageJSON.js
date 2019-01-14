0e90e8f2afa3131f030713454817a4ac6e461062
bordoni <bordoni.dev@gmail.com>
2019-01-13 23:26:41 -0200

    Remove double ;

--- scripts/fetchPackageJSON.js
+++ scripts/fetchPackageJSON.js
@@ -3,14 +3,14 @@ const https = require( 'https' );
 const { execSync } = require( 'child_process' );
 const { spawnSync } = require( 'child_process' );
 let ref = spawnSync( 'git', [ 'rev-parse', '--abbrev-ref', 'HEAD' ], { shell: true, cwd: process.cwd() + '/common' } );
-ref = ref.stdout.toString().trim();;
+ref = ref.stdout.toString().trim();
 
 // If Common is not a branch, it will required a second command
 if ( 'HEAD' === ref ) {
 	ref = spawnSync( 'git', [ 'rev-parse', '--verify', 'HEAD' ], { shell: true, cwd: process.cwd() + '/common' } );
 }
 
-ref = ref.stdout.toString().trim();;
+ref = ref.stdout.toString().trim();
 
 const URL = `https://raw.githubusercontent.com/moderntribe/tribe-common/${ref}/package.json`;
 
