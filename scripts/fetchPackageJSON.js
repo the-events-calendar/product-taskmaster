2cbf059a58e33be29d4699d8c777c6c5f157831f
bordoni <bordoni.dev@gmail.com>
2019-01-18 21:55:31 -0200

    Makes sure we have the correct versioning for common fetcher

--- scripts/fetchPackageJSON.js
+++ scripts/fetchPackageJSON.js
@@ -2,15 +2,22 @@
 const https = require( 'https' );
 const { execSync } = require( 'child_process' );
 const { spawnSync } = require( 'child_process' );
-let ref = spawnSync( 'git', [ 'rev-parse', '--abbrev-ref', 'HEAD' ], { shell: true, cwd: process.cwd() + '/common' } );
-ref = ref.stdout.toString().trim();
+let ref;
 
-// If Common is not a branch, it will required a second command
-if ( 'HEAD' === ref ) {
-	ref = spawnSync( 'git', [ 'rev-parse', '--verify', 'HEAD' ], { shell: true, cwd: process.cwd() + '/common' } );
+if ( fs.existsSync( process.cwd() + '/common' ) ) {
+	ref = spawnSync( 'git', [ 'rev-parse', '--abbrev-ref', 'HEAD' ], { shell: true, cwd: process.cwd() + '/common' } );
+	ref = ref.stdout.toString().trim();
+
+	// If Common is not a branch, it will required a second command
+	if ( 'HEAD' === ref ) {
+		ref = spawnSync( 'git', [ 'rev-parse', '--verify', 'HEAD' ], { shell: true, cwd: process.cwd() + '/common' } );
+	}
+
+	ref = ref.stdout.toString().trim();
+} else {
+	ref = spawnSync( 'git', [ 'rev-parse', '--abbrev-ref', 'HEAD' ], { shell: true, cwd: process.cwd() } );
+	ref = ref.stdout.toString().trim();
 }
-
-ref = ref.stdout.toString().trim();
 
 const URL = `https://raw.githubusercontent.com/moderntribe/tribe-common/${ref}/package.json`;
 
@@ -26,7 +33,6 @@ module.exports = new Promise( ( resolve, reject ) => {
 				console.info( 'Successfully fetched latest tribe-common package.json' );
 			} catch ( e ) {
 				reject( e );
-				console.error( e );
 			}
 		} );
 	} );
