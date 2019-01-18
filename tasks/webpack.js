38a1e0e1f4771ffddb845cd993e5d4bbeaac1396
bordoni <bordoni.dev@gmail.com>
2019-01-18 21:24:07 -0200

    Make sure we have common before we use that command

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -2,6 +2,6 @@ module.exports = function( gulp ) {
 	const run = require( 'gulp-run-command' );
 
 	gulp.task( 'webpack',
-		run.default( [ 'npm run build:common', 'npm run build:webpack' ], { ignoreErrors: true } )
+		run.default( [ '[ -d "common" ] && npm run build:common', 'npm run build:webpack' ], { ignoreErrors: true } )
 	);
 };
