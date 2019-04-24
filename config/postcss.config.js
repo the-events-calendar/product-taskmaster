0ac539553c7304cf7b5dc20808cbf5cef50b0f02
Paul <kim.paul.ms@gmail.com>
2019-04-24 20:02:11 +0800

    make postcss config processor order same as postcss task

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,9 +1,11 @@
 module.exports = ( { file, options, env } ) => ( { // eslint-disable-line no-unused-vars
 	plugins: [
 		require( 'postcss-import' )( { root: file.dirname } ),
-		require( 'postcss-preset-env' ),
-		require( 'postcss-nested' ),
 		require( 'postcss-mixins' ),
+		require( 'postcss-nested' ),
+		require( 'postcss-inline-svg' ),
+		require( 'postcss-preset-env' )( { stage: 0, preserve: false } ),
+		require( 'postcss-calc' ),
 		require( 'postcss-hexrgba' ),
 		require( 'css-mqpacker' ),
 	],
