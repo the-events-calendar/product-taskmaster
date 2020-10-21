3e363d16fe248ddb10dc6678bf0b36add409e7d9
Paul <kim.paul.ms@gmail.com>
2020-10-22 02:49:48 +0800

    reorder requiring of gulp tasks for v4

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ module.exports = function( gulp ) {
 	var normalizedPath = require( 'path' ).join( __dirname, 'tasks' );
 
 	require( 'fs' ).readdirSync( normalizedPath ).forEach( function( file ) {
+		if ( 'default.js' === file || 'package.js' === file ) {
+			return;
+		}
+
 		require( './tasks/' + file )( gulp );
 	} );
+
+	// Gulp v4 must have tasks that are used in series() or parallel() defined first.
+	require( './tasks/default.js' )( gulp );
+	require( './tasks/package.js' )( gulp );
 };
