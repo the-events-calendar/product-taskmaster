d8bf40e675af0519402467022f2320d9882ad5d0
Paul <kim.paul.ms@gmail.com>
2020-12-29 01:16:19 +0800

    make tasks v4 only

--- tasks/package.js
+++ tasks/package.js
@@ -1,38 +1,16 @@
 module.exports = function( gulp ) {
 	'use strict';
 
-	var task;
-
-	// Gulp is v3.
-	if ( gulp.hasTask ) {
-		var sequence = require( 'run-sequence' ).use( gulp );
-		task = function( callback ) {
-			sequence(
-				'pull',
-				'postcss',
-				[
-					'compress-js',
-					'compress-css'
-				],
-				'webpack',
-				'zip',
-				callback
-			);
-		};
-
-	// Gulp is v4.
-	} else {
-		task = gulp.series(
-			'pull',
-			'postcss',
-			gulp.parallel(
-				'compress-js',
-				'compress-css',
-			),
-			'webpack',
-			'zip',
-		);
-	}
+	var task = gulp.series(
+		'pull',
+		'postcss',
+		gulp.parallel(
+			'compress-js',
+			'compress-css',
+		),
+		'webpack',
+		'zip',
+	);
 
 	gulp.task( 'package', task );
 };
