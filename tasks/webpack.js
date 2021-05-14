2e76c73b182bc4a9ed30d09d9543bd79f5f04727
Paul <kim.paul.ms@gmail.com>
2021-05-14 23:04:50 +0800

    update npm commands to yarn

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -2,6 +2,6 @@ module.exports = function( gulp ) {
 	const run = require( 'gulp-run-command' );
 
 	gulp.task( 'webpack',
-		run.default( [ 'npm run build:common', 'npm run build:webpack' ], { ignoreErrors: true } )
+		run.default( [ 'yarn run build:common', 'yarn run build:webpack' ], { ignoreErrors: true } )
 	);
 };
