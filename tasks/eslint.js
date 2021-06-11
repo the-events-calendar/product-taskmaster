66f135275ebbaed596b72765e9206a7074a3cf45
Paul <kim.paul.ms@gmail.com>
2021-06-11 15:36:07 +0800

    update eslint task

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -4,17 +4,27 @@ module.exports = function( originalGulp ) {
 	var gulp = require( 'gulp-param' )( originalGulp, process.argv );
 	var eslint = require( 'gulp-eslint' );
 
-	var task = function( path ) {
-		if ( typeof path !== 'string' || ! path ) {
+	var task = function( basePath, fix ) {
+		if ( typeof basePath !== 'string' || ! basePath ) {
+			console.error( '`--basePath` flag must be provided' );
 			process.exit(-1);
 		}
 
-		return gulp.src( path )
-			.pipe( eslint( {
-				resolvePluginsRelativeTo: 'node_modules/product-taskmaster',
-			} ) )
+		var config = {
+			resolvePluginsRelativeTo: 'node_modules/product-taskmaster',
+		};
+
+		if ( typeof fix === 'boolean' ) {
+			config.fix = fix;
+		}
+
+		var filePath = basePath + '/**/*.js';
+
+		return gulp.src( filePath )
+			.pipe( eslint( config ) )
 			.pipe( eslint.format() )
-			.pipe(eslint.failAfterError());
+			.pipe( eslint.failAfterError() )
+			.pipe( gulp.dest( basePath ) );
 	};
 
 	gulp.task( 'eslint', task );
