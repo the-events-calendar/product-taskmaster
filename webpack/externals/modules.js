3e6fd960d8d6eac6a3a834c2544c121957492786
Paul <kim.paul.ms@gmail.com>
2022-01-21 18:07:12 +0800

    add modules, add regenerator runtime to vendor externals

---
+++ webpack/externals/modules.js
@@ -0,0 +1,8 @@
+const modules = {
+	'prop-types': 'tribe.modules.propTypes',
+	'react-redux': 'tribe.modules.reactRedux',
+	redux: 'tribe.modules.redux',
+	'redux-saga': 'tribe.modules.reduxSaga',
+};
+
+module.exports = modules;
--- webpack/externals/vendor.js
+++ webpack/externals/vendor.js
@@ -2,6 +2,7 @@ const vendor = {
 	moment: 'moment',
 	react: 'React',
 	'react-dom': 'ReactDOM',
+	'regenerator-runtime/runtime': 'regeneratorRuntime',
 };
 
 module.exports = vendor;
