c7f7fe485ffd4521b98f9113db60596e96365d98
Paul <kim.paul.ms@gmail.com>
2022-01-21 20:51:15 +0800

    add more common modules to list

--- webpack/externals/modules.js
+++ webpack/externals/modules.js
@@ -1,8 +1,14 @@
 const modules = {
+	classnames: 'tribe.modules.classnames',
 	'prop-types': 'tribe.modules.propTypes',
+	'react-input-autosize': 'tribe.modules.reactInputAutosize',
+	'react-day-picker': 'tribe.modules.reactDayPicker',
 	'react-redux': 'tribe.modules.reactRedux',
 	redux: 'tribe.modules.redux',
 	'redux-saga': 'tribe.modules.reduxSaga',
+	'redux-saga/effects': 'tribe.modules.reduxSaga.effects',
+	'redux-saga/utils': 'tribe.modules.reduxSaga.utils',
+	reselect: 'tribe.modules.reselect',
 };
 
 module.exports = modules;
