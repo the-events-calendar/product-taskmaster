a1a1b629987e906044ff1bd058909afd007c8a48
Paul <kim.paul.ms@gmail.com>
2022-02-16 22:01:36 +0800

    fix lodash external function name

--- webpack/externals/lodash.js
+++ webpack/externals/lodash.js
@@ -1,6 +1,6 @@
 const REGEX = /^lodash/;
 
-module.exports = function commonExternals( context, request, callback ) {
+module.exports = function lodashExternals( context, request, callback ) {
 	if ( REGEX.test( request ) ) {
 		return callback( null, request.replace( /\//g, '.' ) );
 	}
