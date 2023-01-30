bba8c4691b7e4b8971ff42bba0df802fb7aceb6b
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-01-30 14:36:41 -0500

    Add a JS preamble to our minified files

--- tasks/compress-js.js
+++ tasks/compress-js.js
@@ -5,6 +5,20 @@ module.exports = ( gulp, pkg ) => {
 	const terser = require('gulp-terser');
 	var rename = require( 'gulp-rename' );
 
+	const banner = [
+		'/**',
+		' * This JS file was auto-generated via Terser',
+		' *',
+		' * Contributors should avoid editing this file, but instead edit the associated',
+		' * non minified file file. For more information, check out our engineering',
+		' * docs on how we handle JS minification in our engineering docs.',
+		' *',
+		' * @see: https://evnt.is/dev-docs',
+		' */',
+		'',
+		'',
+	].join( '\n' );
+
 	var task = function() {
 		var dir = 'src/resources/js';
 
@@ -20,7 +34,10 @@ module.exports = ( gulp, pkg ) => {
 			] )
 			.pipe( terser( {
 				keep_fnames: true,
-				mangle: false
+				mangle: false,
+				format: {
+					preamble: banner,
+				}
 			} ) )
 			.pipe(
 				rename( {
