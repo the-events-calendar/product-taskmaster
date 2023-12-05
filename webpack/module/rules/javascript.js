61121f4a85e89611b0cd9fb881778848b98d7b77
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-12-05 12:41:18 -0500

    Fix indentation to tabs

--- webpack/module/rules/javascript.js
+++ webpack/module/rules/javascript.js
@@ -1,12 +1,12 @@
 module.exports = {
 	test: /\.js$/,
 	exclude: {
-      and: [ /node_modules/ ], // Exclude libraries in node_modules ...
-      not: [
-        // Except for a few of them that needs to be transpiled because they use modern syntax
-        /@the-events-calendar/,
-        /@moderntribe/,
-      ]
+		and: [ /node_modules/ ], // Exclude libraries in node_modules ...
+		not: [
+			// Except for a few of them that needs to be transpiled because they use modern syntax
+			/@the-events-calendar/,
+			/@moderntribe/,
+		]
     },
 	use: 'babel-loader',
 };
